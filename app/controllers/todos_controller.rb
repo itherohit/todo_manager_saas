9f0eff396ba0d2e094e551fc5669888e3cdc0662
itherohit <rohitshan751@gmail.com>
2020-03-10 18:16:41 +0530

    Post Request verified

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,25 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.all.map {|todo| todo.to_string}.join("\n")
   end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: false,
+    )
+    render plain: "New Todo Created with the id #{new_todo.id}"
+  end
+
   def show
     id=params[:id]
     todo=Todo.find(id)
     render plain: todo.to_string
   end
+
 end
