d1b9dbd28dc8711e76dc3a4be1b072832ad1aea9
itherohit <rohitshan751@gmail.com>
2020-04-02 19:16:14 +0530

    ERB Added

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,18 @@
 class Todo < ActiveRecord::Base
-  def to_string
-    is_completed = completed ? "[X]" : "[ ]"
-    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+  
+  def self.completed
+    all.where(completed: true)
   end
 end
+    
